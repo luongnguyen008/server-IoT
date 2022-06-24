c4b239c074c39332d87e96410f1db01f5557150c
Luong Nguyen <luongnguyenhoab@gmail.com>
2022-06-24 18:19:05 +0700

    5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const { WebSocketServer } = require('ws');
+const { WebSocketServer, WebSocket } = require('ws');
 
 const server = express()
   .use(express.static(path.join(__dirname, 'public')))
